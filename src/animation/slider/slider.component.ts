578513d64a149f07210bfaa137ce14558a970384
EL <elicloudh@gmail.com>
2019-08-14 23:45:13 +0800

    slider component

--- src/animation/slider/slider.component.ts
+++ src/animation/slider/slider.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, ElementRef, Renderer2, OnDestroy, OnChanges, SimpleChanges, ViewChild, HostListener } from '@angular/core';
+import { Component, Input, OnInit, ElementRef, Renderer2, OnDestroy, OnChanges, SimpleChanges, ViewChild, AfterViewInit } from '@angular/core';
 import { takeUntil } from 'rxjs/operators';
 import { Subject } from 'rxjs';
 import { Orientations } from './slider_orientation';
@@ -8,34 +8,36 @@ import { Orientations } from './slider_orientation';
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.scss']
 })
-export class SliderComponent implements OnInit, OnChanges, OnDestroy {
+export class SliderComponent implements OnInit, OnChanges, OnDestroy, AfterViewInit {
   Effects = Orientations;
 
   effect$ = new Subject<Orientations>();
   stop$ = new Subject<void>();
   constructor(public elementRef: ElementRef, private renderer: Renderer2) {
     this.effect$.pipe(takeUntil(this.stop$)).subscribe(orientation => {
+      const className = 'effect-' + orientation.toString();
       this.renderer.addClass(
         this.elementRef.nativeElement,
-        'effect-' + orientation.toString()
+        className
       );
-      this.renderer.setStyle(this.elementRef.nativeElement, 'width', `${this.containerWidth}px`);
-      this.renderer.setStyle(this.elementRef.nativeElement, 'height', `${this.containerHeight}px`);
+      this.renderer.setStyle(this.elementRef.nativeElement, 'width', `${this.width}px`);
+      this.renderer.setStyle(this.elementRef.nativeElement, 'height', `${this.height}px`);
     });
   }
 
   @Input() orientation = Orientations.Horizontal;
   @Input() start = 50;
+  @Input() labelVisibility = "normal";
+  @Input() width = 600;
+  @Input() height = 400;
+
   @ViewChild('scroller')
   scroller: ElementRef;
 
-  containerWidth = 600;
-  containerHeight = 400;
-
   scrollX = 50;
   scrollY = 50;
-  afterWidth = 100;
-  afterHeight = 100;
+  afterWidth = 0;
+  afterHeight = 0;
 
   moving = false;
 
@@ -56,15 +58,15 @@ export class SliderComponent implements OnInit, OnChanges, OnDestroy {
     if (this.orientation === Orientations.Horizontal) {
       if (y < 0)
         y = 0;
-      if (y > this.containerHeight)
-        y = this.containerHeight;
+      if (y > this.height)
+        y = this.height;
       this.scrollY = y - 15;
       this.afterHeight = y;
     } else {
       if (x < 0)
         x = 0;
-      if (x > this.containerWidth)
-        x = this.containerWidth;
+      if (x > this.width)
+        x = this.width;
       this.scrollX = x - 15;
       this.afterWidth = x;
     }
@@ -77,25 +79,35 @@ export class SliderComponent implements OnInit, OnChanges, OnDestroy {
   ngOnInit() {
     this.effect$.next(this.orientation);
     if (this.orientation === Orientations.Horizontal) {
-      this.afterHeight = this.containerHeight * (this.start / 100);
-      this.afterWidth = this.containerWidth;
-      this.scrollX = this.containerWidth / 2 - 15;
-      this.scrollY = this.containerHeight * (this.start / 100) - 15;
-    } else {
-      this.afterWidth = this.containerWidth * (this.start / 100);
-      this.afterHeight = this.containerHeight;
-      this.scrollY = this.containerHeight / 2 - 15;
-      this.scrollX = this.containerWidth * (this.start / 100) - 15;
+      this.afterHeight = this.height * (this.start / 100);
+      this.afterWidth = this.width;
+      this.scrollX = this.width / 2 - 15;
+      this.scrollY = this.height * (this.start / 100) - 15;
+    }
+    if (this.orientation === Orientations.Vertical) {
+      this.afterWidth = this.width * (this.start / 100);
+      this.afterHeight = this.height;
+      this.scrollY = this.height / 2 - 15;
+      this.scrollX = this.width * (this.start / 100) - 15;
     }
 
     document.addEventListener('mousedown', this.mouseDown.bind(this));
     document.addEventListener('mousemove', this.mouseMove.bind(this));
     document.addEventListener('mouseup', this.mouseUp.bind(this));
   }
+
+  ngAfterViewInit() {
+    document.querySelectorAll(`.effect-${this.orientation} img`).forEach(item => {
+      this.renderer.setAttribute(item, 'width', `${this.width}px`);
+      this.renderer.setAttribute(item, 'height', `${this.height}px`);
+    });
+  }
+
   ngOnDestroy(): void {
     this.stop$.next();
     this.stop$.complete();
   }
+
   ngOnChanges(changes: SimpleChanges): void {
     if (changes.effect) {
       this.effect$.next(changes.orientation.currentValue);
